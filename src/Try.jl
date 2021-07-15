0d0b0d2ee0c5dd2af8728d5672f99d8bd50ba081
Stephan Sahm <stephan.sahm@gmx.de>
2021-07-15 08:45:36 +0200

    document auto-merging of Multiple Exceptions

--- src/Try.jl
+++ src/Try.jl
@@ -54,6 +54,8 @@ end
     MultipleExceptions(tuple_or_vector_of_exceptions)
 
 Little helper type which combines several Exceptions into one new Exception.
+
+In the several arguments version, and only there, if an MultipleExceptions is given, it will be flattened directly for convenience.
 """
 struct MultipleExceptions{Es<:Tuple} <: Exception
   exceptions::Es
